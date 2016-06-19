f3ba6fd586b1a037ef33087855bdcb7f23dc6dbd
Artium <artium@nihamkin.com>
2016-06-19 22:53:16 +0300

    Fixed windows client missing dlls, fixed info page error when id>9

--- counting_site/counting_app/urls.py
+++ counting_site/counting_app/urls.py
@@ -4,7 +4,7 @@ from counting_app import views
 
 urlpatterns = [
     url(r'^info$', views.info_dir, name='info_dir'),
-    url(r'^info/([0-9])$', views.info, name='info'),
+    url(r'^info/([0-9]+)$', views.info, name='info'),
     url(r'^allocate$', views.allocate, name='allocate'),
     url(r'^report$', views.report, name='report'),
 ]
